d9275ed407389adb1b385b8f03d19adf4f5493bb
francis.rammeloo <francis.rammeloo@2481bc01-1f28-e723-b76a-7fb9bf1b6a88>
2013-04-12 08:13:23 +0000

    Fix icons.

--- trunk/pack.sh
+++ trunk/pack.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -rf anime-ratings
 mkdir anime-ratings
-zip anime-ratings.zip icon128.png icon16.png icon48.png background.html content.js manifest.json
+zip anime-ratings.zip icon128.png icon16.png icon48.png content.js eventPage.js manifest.json
 rm -rf anime-ratings
